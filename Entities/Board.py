af025f16f6e44c39865e801f51cf2462870d1f11
Dick Kluis <dkluis@icloud.com>
2020-12-08 16:12:48 -0500

    Board class coding

--- Entities/Board.py
+++ Entities/Board.py
@@ -1,21 +1,21 @@
 from math import sqrt
-from dearpygui.core import does_item_exist, draw_polygon, add_drawing
+from dearpygui.core import does_item_exist, draw_polygon, add_drawing, \
+    draw_text, draw_circle, add_value, set_value, get_value
 
 from Utils import logging
 
 
 class Hexagon:
     def __init__(self,
-                 id_handle,
-                 window_handle,
-                 drawing_handle,
+                 id_handle=int,
+                 window_handle=str,
+                 drawing_handle=str,
                  logfile=logging,
                  size=20,
-                 thickness=13,
-                 hor_offset=100.0,
-                 ver_offset=50.0,
-                 outline_color=[255, 0, 0, 255],
-                 background_color=[]):
+                 thickness=1,
+                 hor_offset=0.0,
+                 ver_offset=0.0,
+                 outline_color=[255, 0, 0, 255]):
         self.id = id_handle
         self.window = window_handle
         self.drawing = drawing_handle
@@ -23,11 +23,6 @@ class Hexagon:
         self.thickness = thickness
         self.horizontal_offset = hor_offset
         self.vertical_offset = ver_offset
-        if len(background_color) == 0:
-            self.use_background_color = False
-        else:
-            self.use_background_color = True
-        self.background_color = background_color
         self.outline_color = outline_color
         self.coordinate_a = [0.0, 0.0]
         self.coordinate_b = [0.0, 0.0]
@@ -38,6 +33,8 @@ class Hexagon:
         self.center = [0.0, 0.0]
         self.top_bot_dist = 0.0
         self.left_right_dist = 0.0
+        self.filled = False
+        self.circled = False
         self.__calc_parameters()
         self.logfile = logfile
     
@@ -50,22 +47,66 @@ class Hexagon:
             3 * self.size) / 2 + self.vertical_offset
         self.coordinate_f = self.size / 2 + self.horizontal_offset, self.size + self.size - sqrt(
             3 * self.size) / 2 + self.vertical_offset
-        # print(self.coordinate_a, self.coordinate_b, self.coordinate_c,
+        # self.logfile.write(self.coordinate_a, self.coordinate_b, self.coordinate_c,
         #      self.coordinate_d, self.coordinate_e, self.coordinate_f, self.coordinate_a)
-        # ToDo calc center
         self.top_bot_dist = sqrt(pow(self.coordinate_b[0] - self.coordinate_f[0], 2) +
                                  pow(self.coordinate_b[1] - self.coordinate_f[1], 2))
-        self.left_right_dist = sqrt(pow(self.coordinate_f[0] - self.coordinate_e[0], 2) +
-                                    pow(self.coordinate_f[1] - self.coordinate_e[1], 2))
-        # print(f'tb {self.top_bot_dist}, lr {self.left_right_dist}')
+        self.left_right_dist = (self.size + self.size / 2) / 2
+        self.center = (self.coordinate_a[0] - self.coordinate_d[0]) / 2 + self.coordinate_d[0], \
+                      (self.coordinate_e[1] - self.coordinate_c[1]) / 2 + self.coordinate_c[1]
+        # self.logfile.write(self.top_bot_dist, self.left_right_dist, self.center)
     
     def draw(self):
         self.__calc_parameters()
-        print('Draw', self.id, self.drawing)
-        if not does_item_exist(self.drawing):
-            print("Does not exit", self.drawing)
+        if not does_item_exist(self.drawing) and self.id != 0:
             self.logfile.write(f'The Drawing Handle does not exist', 0)
             add_drawing(self.drawing, parent=self.window, width=1100, height=1100)
         draw_polygon(self.drawing, [self.coordinate_a, self.coordinate_b, self.coordinate_c, self.coordinate_d,
                                     self.coordinate_e, self.coordinate_f, self.coordinate_a],
                      self.outline_color, thickness=self.thickness, tag=self.id)
+        self.logfile.write(f'Drawing Hexagon {self.id} with tag {self.id}')
+        
+    def fill(self, fillcolor=[255, 255, 255, 255]):
+        draw_polygon(self.drawing, [self.coordinate_a, self.coordinate_b, self.coordinate_c, self.coordinate_d,
+                                    self.coordinate_e, self.coordinate_f, self.coordinate_a],
+                     self.outline_color, thickness=0, fill=fillcolor, tag=self.id)
+        self.filled = True
+        self.logfile.write(f'Filling Hexagon {self.id} with tag {self.id} and color {fillcolor}')
+        
+    def draw_cirle(self, circle_color=[]):
+        if len(circle_color) == 0:
+            circle_color = self.outline_color
+        draw_circle(self.drawing, self.center, radius=5.0, color=circle_color, tag=f'C{self.id}')
+        self.circled = True
+        self.logfile.write(f'Circle in middle of Hexagon {self.id} with tag {self.id} and color {circle_color}')
+        
+    def report(self):
+        self.logfile.write(f'Reporting all coordinate on {self.id} starting with center: {self.center}, '
+                           f'{self.coordinate_a}, {self.coordinate_b}, {self.coordinate_c}, '
+                           f'{self.coordinate_d}, {self.coordinate_e}, {self.coordinate_f}, '
+                           f'Circled: {self.circled} and Filled {self.filled}')
+
+
+class Board:
+    def __init__(self, log=logging):
+        self.dict = {}
+        self.logfile = log
+        self.logfile.write('Created Board Dictionary')
+    
+    def add(self, k, v):
+        kv = {f"{k}": v}
+        self.dict.update(kv)
+        self.logfile.write(f'Added {kv} and dict is now {len(self.dict)} long')
+        
+    def get(self, k):
+        entry = self.dict.get(k)
+        self.logfile.write(f'Getting {k} and found {entry}')
+        return entry
+    
+    def items(self):
+        result = []
+        for k, v in self.dict.items():
+            result.append(v)
+        self.logfile.write(f'Returning all items of the Board Dictionary: {len(result)}')
+        return result
+
--- board.py
+++ board.py
@@ -3,7 +3,7 @@ from Utils import logging
 from dearpygui.core import *
 from dearpygui.simple import *
 
-log = logging(caller='board.py', filename='board')
+log = logging(caller='board.py', filename='TheGame')
 
 
 def do_hexagon_draw(sender, data):
@@ -12,37 +12,39 @@ def do_hexagon_draw(sender, data):
     hexagon_info = Hexagon('info', 'Main', 'Board', log, size=hex_size)
     top_bot_dist = hexagon_info.top_bot_dist
     left_right_dist = (hex_size + hex_size / 2) / 2
-    row_height = int(1000/top_bot_dist)
-    row_len = int(1000/(left_right_dist * 2))
-    col_len = int(1000/top_bot_dist)
-
+    row_height = int(1000 / top_bot_dist)
+    row_len = int(1000 / (left_right_dist * 2))
+    col_len = int(1000 / top_bot_dist)
+    
+    print(top_bot_dist, left_right_dist)
+    
     for idx in range(0, row_height):
         vo = top_bot_dist * idx
-        ho = left_right_dist * 0
+        ho = left_right_dist * 0 + hexagon_info.vertical_offset
         board_array.append(Hexagon(f'A{idx}', 'Main', 'Board', log, size=hex_size,
                                    ver_offset=vo, hor_offset=ho, thickness=1))
         board_array[idx].draw()
     print(len(board_array))
-
+    
     for idx in range(0, row_height):
         vo = top_bot_dist * idx
-        ho = left_right_dist * 2
+        ho = left_right_dist * 2 + hexagon_info.vertical_offset
         board_array.append(Hexagon(f'B{idx}', 'Main', 'Board', log, size=hex_size,
                                    ver_offset=vo + top_bot_dist / 2, hor_offset=ho, thickness=1))
         board_array[idx + row_height].draw()
     print(len(board_array))
-
+    
     for idx in range(0, row_height):
         vo = top_bot_dist * idx
-        ho = left_right_dist * 4
+        ho = left_right_dist * 4 + hexagon_info.vertical_offset
         board_array.append(Hexagon(f'C{idx}', 'Main', 'Board', log, size=hex_size,
                                    ver_offset=vo, hor_offset=ho, thickness=1))
         board_array[idx + row_height * 2].draw()
     print(len(board_array))
-
+    
     for idx in range(0, row_height):
         vo = top_bot_dist * idx
-        ho = left_right_dist * 6
+        ho = left_right_dist * 6 + hexagon_info.vertical_offset
         board_array.append(Hexagon(f'D{idx}', 'Main', 'Board', log, size=hex_size,
                                    ver_offset=vo + top_bot_dist / 2, hor_offset=ho, thickness=1))
         board_array[idx + row_height * 3].draw()
@@ -50,20 +52,79 @@ def do_hexagon_draw(sender, data):
     
     for idx in range(0, row_height):
         vo = top_bot_dist * idx
-        ho = left_right_dist * 8
+        ho = left_right_dist * 8 + hexagon_info.vertical_offset
         board_array.append(Hexagon(f'E{idx}', 'Main', 'Board', log, size=hex_size,
                                    ver_offset=vo, hor_offset=ho, thickness=1))
         board_array[idx + row_height * 4].draw()
     print(len(board_array))
     
     print(f'row len {row_len}, column len {col_len}')
+
+
+def do_board_draw_example():
+    board = Board(log)
+    hex_size = 30
+    
+    hexagon_info = Hexagon('info', '', '', log, size=hex_size, thickness=1)
+    top_bot_dist = hexagon_info.top_bot_dist
+    left_right_dist = hexagon_info.left_right_dist
+    vertical_offset = hexagon_info.vertical_offset
+    horizontal_offset = hexagon_info.horizontal_offset
+    log.write(f'Top Bottom distance {top_bot_dist} and Left Right distance {left_right_dist}')
+    log.write(f'Vertical offset {vertical_offset}, Horizontal offset {horizontal_offset}')
+    del hexagon_info
+    
+    hexagon_1 = Hexagon('00#00', '', '', log, size=hex_size, thickness=1)
+    hexagon_2 = Hexagon('01#12', '', '', log, size=hex_size, thickness=2, hor_offset=300, ver_offset=300)
+    board.add('info', hexagon_1)
+    board.add('info2', hexagon_2)
+    result = board.items()
+    for hexagon in result:
+        hexagon.draw()
+        
+    hexagon_1.fill()
+    hexagon_2.draw_cirle([100, 255, 100, 100])
+    hexagon_1.draw_cirle()
+    hexagon_1.report()
+    hexagon_2.report()
+
+
+def do_board_draw():
+    board = Board(log)
+
+    hex_size = 30
+    hexagon_info = Hexagon('info', '', '', log, size=hex_size, thickness=1)
+    top_bot_dist = hexagon_info.top_bot_dist
+    left_right_dist = (hex_size + hex_size / 2) / 2
+    vertical_offset = hexagon_info.vertical_offset
+    horizontal_offset = hexagon_info.horizontal_offset
+    log.write(f'Top Bottom distance {top_bot_dist} and Left Right distance {left_right_dist}')
+    log.write(f'Vertical offset {vertical_offset}, Horizontal offset {horizontal_offset}')
+    # del hexagon_info
+    
+    for row in range(0, 5):
+        ho = (left_right_dist + hex_size) * (row + 1) + hex_size
+        for col in range(0, 5):
+            k = f'{row}##{col}'
+            vo = top_bot_dist * (col + 1) + top_bot_dist
+            board.add(k,
+                      Hexagon(k, 'Main', 'Board', log,
+                              size=hex_size, thickness=1, hor_offset=ho, ver_offset=vo))
+            board.get(k).draw()
+            board.get(k).report()
+            print(row, col, k, vo, ho)
     
 
 with window('Main', width=1000, height=1000, horizontal_scrollbar=True, x_pos=0, y_pos=0,
             label='The Board'):
     log_info(f'Main')
-    do_hexagon_draw('', 'Main')
-
+    
+    #do_hexagon_draw('', 'Main')
+    do_board_draw()
+    #do_board_draw_example()
+    
+    
+    
 set_main_window_title('The Game')
 set_main_window_size(1500, 1500)
 # show_logger()
